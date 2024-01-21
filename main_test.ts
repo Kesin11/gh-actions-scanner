106a56bd3c095e54fcea2786366b3f82e414fa61
Kenta Kase <kesin1202000@gmail.com>
2024-01-21 11:39:23 +0000

    billableをjobsSummaryと結合

--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,20 @@
 import { assertEquals } from "https://deno.land/std@0.205.0/assert/mod.ts";
-import { add } from "./main.ts";
+import { reduceGroups } from "https://deno.land/std@0.212.0/collections/reduce_groups.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+const sumBy = (records: Record<string, unknown[]>, key: string) => {
+  return reduceGroups(records, (sum, it: any) => sum + it[key], 0);
+};
+
+// 実験のために書いたコードなので消してよい
+Deno.test("sumBy", () => {
+  const billable = [
+    { runner: "UBUNTU", duration_ms: 100 },
+    { runner: "UBUNTU", duration_ms: 200 },
+    { runner: "WINDOWS", duration_ms: 100 },
+  ];
+  const billableGroup = Object.groupBy(billable, (x) => x.runner);
+  assertEquals(sumBy(billableGroup, "duration_ms"), {
+    "UBUNTU": 300,
+    "WINDOWS": 100,
+  });
 });
--- src/github.ts
+++ src/github.ts
@@ -40,6 +40,17 @@ export type RunsSummary = {
   usage: WorkflowRunUsage;
 }[];
 
+type StepsSummary = Record<string, {
+  count: number;
+  durationSecs: {
+    min: number | undefined;
+    median: number | undefined;
+    p80: number | undefined;
+    p90: number | undefined;
+    max: number | undefined;
+  };
+}>;
+
 export function createOctokit(): Octokit {
   const token = Deno.env.get("GITHUB_TOKEN");
   const baseUrl = Deno.env.get("GITHUB_API_URL") ?? "https://api.github.com";
@@ -75,6 +86,22 @@ export async function createRunsSummary(
   });
 
   // Add usage data to each run
+  // Example:
+  // usage: {
+  //   billable: {
+  //     UBUNTU: {
+  //       total_ms: 0,
+  //       jobs: 4,
+  //       job_runs: [
+  //         { job_id: 20474751294, duration_ms: 0 },
+  //         { job_id: 20474751396, duration_ms: 0 },
+  //         { job_id: 20474751516, duration_ms: 0 },
+  //         { job_id: 20474762355, duration_ms: 0 }
+  //       ]
+  //     }
+  //   },
+  //   run_duration_ms: 48000
+  // }
   const promises = runsSummary.map((run) => {
     return octokit.actions.getWorkflowRunUsage({
       owner: run.owner,
@@ -82,21 +109,41 @@ export async function createRunsSummary(
       run_id: run.run_id,
     });
   });
-  const workflowUsages = await Promise.all(promises);
-  if (runsSummary.length !== workflowUsages.length) {
+  const workflowRunUsages = await Promise.all(promises);
+  if (runsSummary.length !== workflowRunUsages.length) {
     throw new Error("runsSummary.length !== workflowUsages.length");
   }
   for (let i = 0; i < runsSummary.length; i++) {
-    runsSummary[i].usage = workflowUsages[i].data;
+    runsSummary[i].usage = workflowRunUsages[i].data;
   }
 
   return runsSummary;
 }
 
+type RunnerType = string;
+type JobsSummary = Record<
+  string,
+  Record<string, {
+    count: number;
+    durationSecs: {
+      min: number | undefined;
+      median: number | undefined;
+      p80: number | undefined;
+      p90: number | undefined;
+      max: number | undefined;
+    };
+    stepsSummary: StepsSummary;
+    billable: Record<RunnerType, { sumDurationMs: number }>;
+  }>
+>;
 export async function createJobsSummary(
   octokit: Octokit,
   runsSummary: RunsSummary,
 ) {
+  const jobsBillableSummary = createJobsBillableSummary(
+    runsSummary.map((run) => run.usage),
+  );
+
   const promises = runsSummary.map((run) => {
     return octokit.actions.listJobsForWorkflowRunAttempt({
       owner: run.owner,
@@ -108,7 +155,6 @@ export async function createJobsSummary(
   const workflowJobs = (await Promise.all(promises)).map((res) =>
     res.data.jobs
   );
-
   const jobs = workflowJobs.flat()
     .map((workflowJob) => {
       return {
@@ -116,18 +162,29 @@ export async function createJobsSummary(
         durationSec: diffSec(workflowJob.started_at, workflowJob.completed_at),
       };
     });
-
   const jobsWorkflowGroup = Object.groupBy(
     jobs,
     (job) => job.workflow_name ?? "",
   );
-  const jobsSummary: Record<string, Record<string, unknown>> = {};
+
+  const jobsSummary: JobsSummary = {};
   for (const [workflowName, jobs] of Object.entries(jobsWorkflowGroup)) {
     const jobsJobGroup = Object.groupBy(jobs, (job) => job.name);
     for (const [jobName, jobs] of Object.entries(jobsJobGroup)) {
       const successJobs = jobs.filter((job) => job.conclusion === "success");
       const isJobsEmpty = successJobs.length === 0;
       const durationSecs = successJobs.map((job) => job.durationSec);
+
+      // TODO: functions化する
+      const jobsBillable = jobs.map((job) => jobsBillableSummary[job.id])
+        .filter((job) => job !== undefined);
+      const jobsBillableSum: Record<string, { sumDurationMs: number }> = {};
+      for (const billable of jobsBillable) {
+        jobsBillableSum[billable.runner] = jobsBillableSum[billable.runner] ??
+          { sumDurationMs: 0 };
+        jobsBillableSum[billable.runner].sumDurationMs += billable.duration_ms;
+      }
+
       jobsSummary[workflowName] = jobsSummary[workflowName] ?? {};
       jobsSummary[workflowName][jobName] = {
         count: jobs.length,
@@ -138,6 +195,7 @@ export async function createJobsSummary(
           p90: isJobsEmpty ? undefined : quantile(durationSecs, 0.9),
           max: isJobsEmpty ? undefined : max(durationSecs),
         },
+        billable: jobsBillableSum,
         stepsSummary: createStepsSummary(jobs),
       };
     }
@@ -145,10 +203,10 @@ export async function createJobsSummary(
   return jobsSummary;
 }
 
-function createStepsSummary(workflowJobs: WorkflowJobs) {
+function createStepsSummary(workflowJobs: WorkflowJobs): StepsSummary {
   const steps = workflowJobs.map((job) => job.steps ?? []).flat();
   const stepsGroup = Object.groupBy(steps, (step) => step.name);
-  const stepsSummary: Record<string, unknown> = {};
+  const stepsSummary: StepsSummary = {};
   for (const [stepName, steps] of Object.entries(stepsGroup)) {
     const successSteps = steps.filter((step) => step.conclusion === "success");
     const isStepsEmpty = successSteps.length === 0;
@@ -167,4 +225,41 @@ function createStepsSummary(workflowJobs: WorkflowJobs) {
     };
   }
   return stepsSummary;
+}
+
+// Example:
+// {
+//  "20474751294": { // job_id
+//   "runner": "UBUNTU",
+//   "duration_ms": 0,
+//  },
+//  "20474751396": {
+//   "runner": "UBUNTU-16",
+//   "duration_ms": 0,
+//  }
+// }
+type JobsBillableSummary = Record<string, {
+  runner: string;
+  duration_ms: number;
+}>;
+function createJobsBillableSummary(
+  workflowRunUsages: WorkflowRunUsage[],
+): JobsBillableSummary {
+  const jobsBillableSummary: Record<
+    string,
+    { runner: string; duration_ms: number }
+  > = {};
+  for (const workflowRunUsage of workflowRunUsages) {
+    for (
+      const [runner, billable] of Object.entries(workflowRunUsage.billable)
+    ) {
+      for (const jobsRuns of billable.job_runs ?? []) {
+        jobsBillableSummary[jobsRuns.job_id] = {
+          runner,
+          duration_ms: jobsRuns.duration_ms,
+        };
+      }
+    }
+  }
+  return jobsBillableSummary;
 }
