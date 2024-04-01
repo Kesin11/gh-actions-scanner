7b54adcb833fb090f5e45968b1df715c98f1c738
Kenta Kase <kesin1202000@gmail.com>
2024-04-01 15:36:32 +0000

    stepsのASTをPOCに追加

--- packages/workflow_model/yaml_ast_poc.ts
+++ packages/workflow_model/yaml_ast_poc.ts
@@ -1,4 +1,9 @@
-import { safeLoad, YamlMap } from "npm:yaml-ast-parser@0.0.43";
+import {
+  safeLoad,
+  YamlMap,
+  YAMLMapping,
+  YAMLSequence,
+} from "npm:yaml-ast-parser@0.0.43";
 import { StructuredSource } from "npm:structured-source@4.0.0";
 
 const yaml = Deno.readTextFileSync(".github/workflows/ci.yaml").toString();
@@ -17,4 +22,17 @@ jobs.forEach((it) => {
   // GItHubのリンクは行単位までらしいのでindexToPositionで十分そう
   console.log(`${it.key}:`, src.indexToPosition(it.start));
   console.log(`${it.key}:`, src.rangeToLocation([it.start, it.end]));
+});
+
+const firstJob = jobsMap.mappings[0].value as YamlMap;
+const steps = firstJob.mappings.find((it) => it.key.value === "steps")
+  ?.value as YAMLSequence; // stepsは必ず存在するので確定でYAMLSequence;
+steps.items.forEach((it) => {
+  console.log(
+    // 1つのstepの中身はMapなのでmappingsで見る必要がある。[0]で適当に最初にkeyをここでは表示させてるだけ
+    `${it.mappings[0].key.value}:`,
+    src.indexToPosition(it.startPosition),
+  );
+  // 各stepの先頭の行番号を表示する
+  console.log(src.rangeToLocation([it.startPosition, it.endPosition]));
 });
