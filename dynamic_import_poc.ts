4c13e7e1c0ee8c5177e3d80288aa659895cc5786
Kenta Kase <kesin1202000@gmail.com>
2024-04-07 12:55:55 +0000

    完全にdynamicなimportだとcompile後はエラーになる

--- dynamic_import_poc.ts
+++ dynamic_import_poc.ts
@@ -1,20 +1,13 @@
 const arg = Deno.args[0];
 
 let fun;
-if (arg === "221") {
-  console.log("import 221");
-  const { sumOf } = await import(
-    "https://deno.land/std@0.221.0/collections/sum_of.ts"
-  );
-  fun = sumOf;
-} else {
-  console.log("import 218");
-  const { sumOf } = await import(
-    "https://deno.land/std@0.218.0/collections/sum_of.ts"
-  );
-  fun = sumOf;
-}
+console.log(`import std@0.${arg}.0`);
+const std = `std@0.${arg}.0`;
+const { sumOf } = await import(
+  `https://deno.land/${std}/collections/sum_of.ts`
+);
+fun = sumOf;
 
 const foo = [{ foo: 1 }, { foo: 2 }, { foo: 3 }];
-const sumCount = fun(foo, (run) => run.foo);
+const sumCount = fun(foo, (run: any) => run.foo);
 console.log(sumCount);
