23f8f079df1adb6a7834050a320e632cb634a01e
Kenta Kase <kesin1202000@gmail.com>
2024-06-08 17:21:55 +0900

    githubに公開したカスタムruleをimportするPOC

--- actions-scanner.config.ts
+++ actions-scanner.config.ts
@@ -1,14 +1,12 @@
 // ユーザーが用意するconfig fileのサンプル
+// 自作のruleの.tsをgithubで公開すればそれをimport可能
 
-// import { reportCacheList } from "./rules/cache_list.ts";
-// import { reportActiveCache } from "./rules/cache_active_size.ts";
-import { reportWorkflowUsage } from "./src/rules/workflow_run_usage.ts";
-// import { workflowCountStat } from "./rules/workflow_count_stat.ts";
-// import { reportWorkflowRetryRuns } from "./rules/workflow_retry_runs.ts";
-// import { checkSlowArtifactAction } from "./rules/step_actions_artifact_outdated.ts";
-// import { checkCheckoutFilterBlobNone } from "./rules/step_actions_checkout_depth0.ts";
-// import { checkTooShortBillableJob } from "./rules/job_too_short_billable_runner.ts";
-import { RuleFunc } from "./src/rules/types.ts";
+// プライベートリポジトリの場合はPATをセットしておく必要がある
+// export DENO_AUTH_TOKENS=$(gh auth token)@raw.githubusercontent.com
+// ref: https://docs.deno.com/runtime/manual/basics/modules/private
+
+import { reportWorkflowUsage } from "https://raw.githubusercontent.com/kesin11-private/gh-actions-scanner/dynamic_import_rules/src/rules/workflow_run_usage.ts";
+import { RuleFunc } from "https://raw.githubusercontent.com/kesin11-private/gh-actions-scanner/dynamic_import_rules/src/rules/types.ts";
 
 type Config = {
   rules: RuleFunc[];
