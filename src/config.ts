d2bfc375ca3bd85531d91402eddaaba6a58aadaa
Kenta Kase <kesin1202000@gmail.com>
2024-06-13 15:14:03 +0000

    loadConfig()のtry-catchをneverthrowで書き直し

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 import { existsSync } from "https://deno.land/std@0.224.0/fs/exists.ts";
-import { err, ok, Result } from "npm:neverthrow@6.2.2";
+import { fromPromise, type Result } from "npm:neverthrow@6.2.2";
 import type { Config } from "./rules/types.ts";
 
 type ConfigFile = {
@@ -15,36 +15,33 @@ export async function loadConfig(
   if (configPath === undefined) {
     // Load actions-scanner.config.ts if exists.
     if (existsSync(DEFAULT_USER_CONFIG_FILE)) {
-      try {
-        const absConfigPath = await Deno.realPath(DEFAULT_USER_CONFIG_FILE);
-        console.debug(`Try loading ${absConfigPath} config.`);
-        const config: ConfigFile = await import(absConfigPath);
-
-        return ok(config.default);
-      } catch (error) {
-        return err(
-          new Error(`Failed to load ${DEFAULT_USER_CONFIG_FILE}.`, {
-            cause: error,
-          }),
-        );
-      }
+      return await fromPromise(
+        Deno.realPath(DEFAULT_USER_CONFIG_FILE),
+        (e) => e as Error,
+      )
+        .andThen((absConfigPath) => {
+          console.debug(`Try loading ${absConfigPath} config.`);
+          const config: Promise<ConfigFile> = import(absConfigPath);
+          return fromPromise(config, (e) => e as Error);
+        })
+        .map((config) => config.default);
     }
 
     // Load included default config.
     console.debug(`Load included default config.`);
-    const config = await import("./config_default.ts");
-
-    return ok(config.default);
+    return await fromPromise(import("./config_default.ts"), (e) => e as Error)
+      .map((config) => config.default);
   }
 
   // case: set --config option
-  try {
-    const absConfigPath = await Deno.realPath(configPath);
-    console.debug(`Try loading ${absConfigPath} config.`);
-    const config: ConfigFile = await import(absConfigPath);
-
-    return ok(config.default);
-  } catch (error) {
-    return err(new Error(`Failed to load ${configPath}.`, { cause: error }));
-  }
+  return await fromPromise(Deno.realPath(configPath), (e) => e as Error)
+    .andThen((absConfigPath) => {
+      console.debug(`Try loading ${absConfigPath} config.`);
+      const config: Promise<ConfigFile> = import(absConfigPath);
+      return fromPromise(config, (e) => e as Error);
+    })
+    .map((config) => config.default)
+    .mapErr((error) =>
+      new Error(`Failed to load ${configPath}.`, { cause: error })
+    );
 }
