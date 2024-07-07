5bc1fa728c963cd12b004a72b0080c119f00af5b
Kenta Kase <kesin1202000@gmail.com>
2024-07-07 07:14:51 +0000

    Narrowing scan date range when omit --created

--- src/github_util.ts
+++ src/github_util.ts
@@ -1,11 +1,6 @@
 import type { WorkflowRun } from "../packages/github/github.ts";
 
-// workflowRunsの中で最も古いcreatedを取得し、
-// 今日なら一日前、
-// 昨日なら1週間前、
-// 1週間以上前なら一ヶ月前、
-// 一ヶ月以上前でも一ヶ月前、
-// のcreatedの文字列を返す関数
+// Generate a date range based on the oldest "created_at" value in workflowRuns
 export function generateCreatedDate(workflowRuns: WorkflowRun[]): string {
   const newestDate = new Date(workflowRuns[0].created_at);
   const oldestDate = new Date(workflowRuns.at(-1)!.created_at);
@@ -15,10 +10,18 @@ export function generateCreatedDate(workflowRuns: WorkflowRun[]): string {
 
   if (diffDays === 0) {
     return `>=${newestDate.toISOString().split("T")[0]}`;
+  } else if (diffDays <= 1) {
+    const threeDaysAgo = new Date(newestDate);
+    threeDaysAgo.setDate(newestDate.getDate() - 3);
+    return `>=${threeDaysAgo.toISOString().split("T")[0]}`;
   } else if (diffDays <= 6) {
     const weekAgo = new Date(newestDate);
     weekAgo.setDate(newestDate.getDate() - 7);
     return `>=${weekAgo.toISOString().split("T")[0]}`;
+  } else if (diffDays <= 7) {
+    const twoWeeksAgo = new Date(newestDate);
+    twoWeeksAgo.setDate(newestDate.getDate() - 14);
+    return `>=${twoWeeksAgo.toISOString().split("T")[0]}`;
   } else {
     const monthAgo = new Date(newestDate);
     monthAgo.setMonth(newestDate.getMonth() - 1);
